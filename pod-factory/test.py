117f013479036f0b52bb99913b1e2588a325fca8
Yuan Ren <reyren179@gmail.com>
2020-05-27 15:46:15 +0800

    single yaml file crete

--- pod-factory/test.py
+++ pod-factory/test.py
@@ -27,32 +27,47 @@ def generate_yaml_doc(yaml_file):
     matchLabels = raw_input("输入想要创建的matchLabels名称(testing-label): ")
     containerName = raw_input("输入想要创建的容器的名称(testing-container): ")
     imageName = raw_input("输入想要创建的image的名称: ")
-
+    commandStr = raw_input("输入想要提前执行的命令: ")
+    gpuNum = raw_input("输入想要使用的GPU个数(0): ")
+    volumeMountsName = raw_input("输入想要挂在的逻辑卷名称(mount-name-testing): ")
+    mountPath = raw_input("输入想要挂在的pod中的目录(/usr/share/horovod): ")
+    pvcName = raw_input("输入要使用的pvc(pod-pvc-volume-1): ")
+    disktype = raw_input("输入想要绑定的node(none, optional: cpu-master gpu-node1/2/3): ")
 
     if deploymentName == "":
         deploymentName = "pod-testing"
     if replicasNum == "":
         replicasNum = "1"
     if matchLabels == "":
-        replicasNum = "testing-label"
+        matchLabels = "testing-label"
     if containerName == "":
-        containerName = testing-container
+        containerName = "testing-container"
     if imageName == "":
-        print('请指定imageName')
+        print('ERROR: 请指定imageName')
         os._exit(1)
+    if commandStr == "":
+        commandStr = "[ \"/bin/bash\", \"-ce\", \"tail -f /dev/null\" ]"
+    if gpuNum == "":
+        gpuNum = 0
+    if volumeMountsName == "":
+        volumeMountsName = "mount-name-testing"
+    if mountPath == "":
+        mountPath = "/usr/share/horovod"
+    if pvcName == "":
+        pvcName = "pod-pvc-volume-1"
 
     py_object = {
                 'apiVersion':'apps/v1',
                 'kind':'Deployment',
                 'metadata':{'name':deploymentName,'namespace':'ai'},
                 'spec':{'replicas':int(replicasNum),
-                        'selector':{'matchLabels':{'run':matchLabels}}
-                        #'template':{'metadata':{'labels':{'run':matchLabels}},
-                        #            'spec':{'containers':{'name':containerName,
-                        #                                  'image':imageName}
-                        #                    }
-                        #            }
-                        }
+                        'selector':{'matchLabels':{'run':matchLabels}},
+                        'template':{'metadata':{'labels':{'run':matchLabels}},
+                            'spec':{'containers':[{'name':containerName,'image':imageName,'command':commandStr, 'resources':{'limits':{'nvidia.com/gpu':int(gpuNum)}},'volumeMounts':[{'name':volumeMountsName, 'mountPath':mountPath}]}],
+                                    'nodeSelector':{'disktype':disktype},
+                                    'volumes':[{'name':volumeMountsName,'persistentVolumeClaim':{'claimName':pvcName}}]}}
+                        },
+                '---'
                 }
     file = open(yaml_file, 'w')
     yaml.dump(py_object, file)
